f8bddf9f9e9e389ae5b98a4931476a155beb0c92
Nivrutti patil <nivrutti.patiil@wwindia.com>
2014-07-17 11:43:46 +0530

    test
    test

---
+++ PRConstants.m
@@ -0,0 +1,13 @@
+//
+//  PRConstants.m
+//  PerformRx
+//
+//  Created by DEREK STOCK on 3/27/14.
+//  Copyright (c) 2014 Tmed. All rights reserved.
+//
+
+#import "PRConstants.h"
+//AUTH_URL @"http://www.raykhman.net/MobileService/MobileService.svc/UserId/pauser/Password/password
+NSString* const REST_URL = @"http://www.raykhman.net/MobileService/MobileService.svc";
+//NSString* const KMHP_URL = @"http://www.raykhman.net/mobileservice/mobileService.svc/Customers/KMHP";
+
